73facbe7bbd652a6db6f3ae7a8d5d5279d20fbb2
fabilu <39083659+fabilu@users.noreply.github.com>
2018-05-08 15:10:38 +0200

    Update roulette_2.ino

--- roulette_2.ino
+++ roulette_2.ino
@@ -1,22 +1,27 @@
 #include <Gamebuino-Meta.h>
 
 int x=0;
-int y=1;
-int z=2;
+int y=0;
+int z=0;
 int a=0;
 int b=0;
 int c=0;
 int argent=100;
+const int buttonPin = 2; 
+const int ledPin =  13;
 
 void setup() {
    gb.begin();
+   srand(9);
+   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
 
 while(!gb.update());
   gb.display.clear();
-    
+
 gb.display.setColor(BLUE);
 gb.display.println("MACHINE A SOUS");
 gb.display.setFontSize(2);
@@ -29,12 +34,21 @@ gb.display.print(a);
 gb.display.print(b);
 gb.display.println(c);
 argent=argent -1;
+gb.sound.playCancel();
+if (x == y && y == z) { argent += 10;
+gb.sound.playOK();
+digitalWrite(ledPin, HIGH); }
+
+else if (x == y || x == z || y == z) { argent += 5;
+gb.sound.playOK();
+digitalWrite(ledPin, HIGH); }
 }
 
-else{
+else if (argent>0){
+  
 x=random(9);
-y=random(x);
-z=random(y);
+y=random(9);
+z=random(9);
 gb.display.print(x);
 gb.display.print(y);
 gb.display.println(z);
@@ -47,9 +61,10 @@ gb.display.setColor(RED);
 gb.display.print(a);
 gb.display.print(b);
 gb.display.println(c);
+ digitalWrite(ledPin, LOW);
 }
 
+if (gb.buttons.pressed(BUTTON_MENU))
+{argent=100;
 }
-
-
-
+}
